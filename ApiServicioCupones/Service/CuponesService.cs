dec6c29b99344d536865b09f186078e0aadbf4d4
JuanIBr85 <juanignaciobauer85@gmail.com>
2024-11-12 23:54:59 -0300

    mejora metodo generar cupon, control cupon duplicado

--- ApiServicioCupones/Service/CuponesService.cs
+++ ApiServicioCupones/Service/CuponesService.cs
@@ -1,18 +1,34 @@
-﻿using ApiServicioCupones.Interfaces;
+﻿using ApiServicioCupones.Data;
+using ApiServicioCupones.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiServicioCupones.Service
 {
     public class CuponesService : ICuponesService
     {
-        public async Task <string> GenerarNroCupon()
+        private readonly DataBaseContext _context;
+
+        public CuponesService(DataBaseContext context)
         {
+            _context = context;
+        }
+
+        public async Task<string> GenerarNroCupon()
+        {
+
             var random = new Random();
 
-            var tresCifras1 = random.Next(0,1000).ToString("D3");
-            var tresCifras2 = random.Next(0,1000).ToString("D3");
-            var tresCifras3 = random.Next(0,1000).ToString("D3");
+            string nroCupon;
+            do
+            {
 
-            var nroCupon = $"{tresCifras1}-{tresCifras2}-{tresCifras3}";
+                var tresCifras1 = random.Next(0, 1000).ToString("D3");
+                var tresCifras2 = random.Next(0, 1000).ToString("D3");
+                var tresCifras3 = random.Next(0, 1000).ToString("D3");
+
+                nroCupon = $"{tresCifras1}-{tresCifras2}-{tresCifras3}";
+
+            } while (await _context.Cupones_Clientes.AnyAsync(c => c.NroCupon == nroCupon));
 
             return nroCupon;
         }
