2d23323973e1bc00b7f62b8e596932e3169d277c
JuanIBr85 <juanignaciobauer85@gmail.com>
2024-11-13 23:11:52 -0300

    conflictos

--- ClienteApi/obj/Debug/net8.0/ClienteApi.AssemblyInfo.cs
+++ ClienteApi/obj/Debug/net8.0/ClienteApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClienteApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63ecf370cc4ab09c18f82854135a357cb1da0084")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deaccf267a0936c106dcdc5761ca153097420cdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClienteApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClienteApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
